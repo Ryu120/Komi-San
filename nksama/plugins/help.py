52437212a62313b8eb7aa08ef3c58fd93553fdc4
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2021-12-11 13:03:11 +0530

    Update help.py

--- nksama/plugins/help.py
+++ nksama/plugins/help.py
@@ -5,13 +5,13 @@ from pyrogram.types.bots_and_keyboards.inline_keyboard_markup import InlineKeybo
 from nksama import bot ,help_message
 from typing import List , Any
 
-HELPP_TEXT = """Yo, Komi here a telegram management bot written on pyrogram library 
+HELPP_TEXT = """Yo, Rengoku here a telegram management bot written on pyrogram library 
 
 Check the following buttons for more info 
 
-Report bugs at - @komisan_support"""
+Report bugs at - @komisansupport"""
 
-@bot.on_message(filters.command('help') | filters.command('help@KomiSanRobot'))
+@bot.on_message(filters.command('help') | filters.command('help@FlameXbot'))
 def bothelp(_,message):
     if message.chat.type == "private":
         keyboard = []
@@ -24,6 +24,6 @@ def bothelp(_,message):
     else:
         bot.send_photo(message.chat.id , "https://telegra.ph/file/769474503795f6d4f406c.jpg" ,  caption=HELPP_TEXT , reply_markup=InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("Pm me for more details"  , url="t.me/komisanrobot?start=help")]
+                [InlineKeyboardButton("Pm me for more details"  , url="t.me/FlameXbot?start=help")]
             
             ]))
